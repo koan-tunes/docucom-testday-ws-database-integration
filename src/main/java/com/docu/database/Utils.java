4adfb7ad1c2e0b4ea5e5c43b8f69d5b54236e48d
Testday <testday@docu.com>
2020-02-10 15:40:10 +0100

    Revert "zwischenspeichern"
    This reverts commit 6915de00

--- src/main/java/com/docu/database/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.docu.database;
-
-public final class Utils {
-	public final static String url = "http://localhost:8080/persons";
-	public final static String dbUrl = "jdbc:postgresql://localhost:5432/docucom_database";
-	public final static String user = "postgres";
-	public final static String password = "";
-
-}
