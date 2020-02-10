ac39a8b3e2a5f22e377ac9fdd231c903dfdb0156
Testday <testday@docu.com>
2020-02-10 15:39:25 +0100

    Revert "zwischenspeichern"
    This reverts commit 3bf108dd

--- src/main/java/com/docu/database/DatabaseUtils.java
+++ src/main/java/com/docu/database/DatabaseUtils.java
@@ -2,8 +2,6 @@ package com.docu.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public final class DatabaseUtils {
 
@@ -11,7 +9,6 @@ public final class DatabaseUtils {
 	private String url = "jdbc:postgresql://localhost:5432/docucom_database";
 	private String user = "postgres";
 	private String password = "";
-	private String query = "select * from person";
 
 
 	/**
@@ -36,18 +33,5 @@ public final class DatabaseUtils {
 
 	public Connection getDbConnection() throws Exception {
 		return getDbConnection(this.driver, this.url, this.user, this.password);
-	}
-
-	private void test () {
-		try  {
-			Connection con = getDbConnection();
- 			PreparedStatement statement = con.prepareStatement(query);
-			ResultSet resultSet = statement.executeQuery();
-
-			//todo ablesen in object speichern
-
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
--- src/main/java/com/docu/database/UserService.java
+++ src/main/java/com/docu/database/UserService.java
@@ -13,34 +13,34 @@ import org.json.JSONArray;
 public class UserService {
 
 	private JSONArray getUsersAsJSON() {
-		JSONArray array = null;
 		try {
-			array = new JSONArray(IOUtils.toString(new URL(Utils.url), Charset.forName("UTF-8")));
+			return new JSONArray(IOUtils.toString(new URL(Utils.url), Charset.forName("UTF-8")));
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		return array;
+		return null;
 	}
 
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<User>();
 		for(Object a : getUsersAsJSON()) {
-			//todo: here mit db zusammenfügen?
+			//wird mit db vergliechen und neue user erstellt
 			users.add(this.readPerson(a.toString()));
 		}
 		return users;
 	}
 
-	private User readPerson(final String json) {
-		User user = null;
+	private User readPerson(String json) {
+		final User user;
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			user = mapper.readValue(json, User.class);
+			return user;
 		} catch(Exception e) {
 			System.err.println(e);
 		}
 
-		return user;
+		return null;
 	}
 
 }
